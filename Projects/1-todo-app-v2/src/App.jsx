17f3519a51659b60b9eadd7a182e835c6171b465
Shyamsunderhait <shyamsunderhait@gmail.com>
2024-05-16 01:58:01 +0530

    addTodo

--- Projects/1-todo-app-v2/src/App.jsx
+++ Projects/1-todo-app-v2/src/App.jsx
@@ -3,9 +3,10 @@ import AddTodo from "./components/AddTodo";
 import TodoItem from "./components/TodoItem";
 import "./App.css";
 import TodoItems from "./components/TodoItems";
+import { useState } from "react";
 
 function App() {
-  const todoItems = [
+  const initialTodoItems = [
     {
       name: "Buy Milk",
       dueDate: "4/12/2334",
@@ -31,12 +32,18 @@ function App() {
       dueDate: "4/12/7774",
     },
   ];
+
+  const [todoItems, setTodoItems] = useState(initialTodoItems);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`new Item added ${itemName} ${itemDueDate}`);
+  };
   return (
     <>
       <center className="todo-container">
         <AppName />
 
-        <AddTodo />
+        <AddTodo onNewItem={handleNewItem} />
         <TodoItems todoItems={todoItems}></TodoItems>
       </center>
     </>
--- Projects/1-todo-app-v2/src/components/AddTodo.jsx
+++ Projects/1-todo-app-v2/src/components/AddTodo.jsx
@@ -1,16 +1,40 @@
-function AddTodo() {
+import { useState } from "react";
+
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState();
+  const [dueDate, setDueDate] = useState();
+
+  const handleNameChange = () => {
+    setTodoName(event.target.value);
+  };
+  const handleDateChange = () => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddBtn = () => {
+    onNewItem(todoName, dueDate);
+  };
+
   return (
     <div className="container text-center">
       <div className="row sh-row">
         <div className="col-6">
-          <input type="text" placeholder="Enter task" />
+          <input
+            type="text"
+            placeholder="Enter task"
+            onChange={handleNameChange}
+          />
         </div>
 
         <div className="col-4">
-          <input type="date" />
+          <input type="date" onChange={handleDateChange} />
         </div>
         <div className="col-2">
-          <button type="button" className="btn btn-success sh-button">
+          <button
+            type="button"
+            className="btn btn-success sh-button"
+            onClick={handleAddBtn}
+          >
             Add
           </button>
         </div>
